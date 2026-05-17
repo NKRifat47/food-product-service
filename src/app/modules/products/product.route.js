cf3a2372517604e69dbd36b240393ba54a522ec3
NKRifat47 <nk.rifat47@gmail.com>
2026-05-17 17:34:21 +0600

    update code

--- src/app/modules/products/product.route.js
+++ src/app/modules/products/product.route.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const productController = require('./product.controller');
-const authMiddleware = require('../../middlewares/auth.js');
 
 router.post('/', productController.createProduct);
 
-router.get("/", authMiddleware, productController.getAllProducts);
+router.get("/", productController.getAllProducts);
 
 module.exports = router;
--- src/app/utils/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const verifyToken = (token) => {
-  return jwt.verify(token, process.env.JWT_SECRET);
-};
-
-module.exports = {
-  verifyToken,
-};
